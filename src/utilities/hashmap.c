29807f8eb6019bcd0584f5c6cbccc1eeddf7911e
bulbasaur <bulbasaur@bulbasaur.com>
2024-01-27 13:09:00 -0500

    Changed gitignore

--- src/utilities/hashmap.c
+++ src/utilities/hashmap.c
@@ -4,14 +4,14 @@
 
 
 
-typedef struct hashmap{
+struct hashmap{
      void* items[CAP];
 } Map;
 
 void put(void*,void*,int);
-void* get(void*);
+void* get(void*,int);
 
-int hash(void* key,int size){
+int hashFun(void* key,int size){
      int hashed = 0;
      unsigned char *p = key;
      for(int i=0;i<size;i++){
@@ -22,11 +22,18 @@ int hash(void* key,int size){
 }
 
 void put(void* key,void* val,int size){
-     printf("%i\n",hash(key,size));
+     int hash = hashFun(key,size);
+     if(Map.items[hash]!=NULL){
+        printf("Filled\n");
+     }else{
+        Map.items[hash] = val;
+     }
 }
 
 
-void* get(void* key){
+void* get(void* key,int size){
+     int hash = hashFun(key,size);
+     return Map.items[hash];
 }
 
 
@@ -35,4 +42,9 @@ int main(){
     int k1 = 1;
     int k2 = 2;
     int k3 = 3;
+    val = 9999;
+    put(&k1,&val,4);
+    put(&k1,&val,4);
+    int res = *((int*)get(&k1,4));
+    printf("%i\n",res);
 }
